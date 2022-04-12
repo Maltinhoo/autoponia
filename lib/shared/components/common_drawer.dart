72a3c05a604b5400e3c14449c87eb38ca059b024
Maltinhoo <malthusim5@gmail.com>
2022-04-11 23:56:16 -0300

    altered name in drawer

--- lib/shared/components/common_drawer.dart
+++ lib/shared/components/common_drawer.dart
@@ -29,7 +29,7 @@ class CommonDrawer extends StatelessWidget {
             ),
           ),
           Text(
-            'José Luis',
+            'Josué',
             style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.bold,
